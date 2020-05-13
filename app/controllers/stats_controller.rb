17a9a517d921880c03d8897928e9b23d3eb83c1a
shayla_white <shayladwhite1@gmail.com>
2020-05-12 22:02:28 -0700

    fixed edit form

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -6,4 +6,35 @@ class StatsController < ApplicationController
         end
         erb :'stats/new'
     end
+    post '/stats' do
+        stat = Stat.create(params)
+        user = Helpers.current_user(session)
+        stat.user = user
+        stat.save
+        redirect to "/users/#{user.id}"
+      end
+
+      get '/stats/:id' do
+        if !Helpers.is_logged_in?(session)
+          redirect '/'
+        end
+        @stats = Stat.find_by(id: params[:id])
+        if !@Stat
+          redirect to '/'
+        end
+        erb :'stats/show'
+      end
+
+
+
+
+      get '/stats/:id/edit' do
+        @stat = Stat.find_by(id: params[:id])
+      if !Helpers.is_logged_in?(session) || !@stat || @stat.user != Helpers.current_user(session)
+        redirect '/'
+      end
+      erb :'/stats/edit'
+    end
+  
+
 end
