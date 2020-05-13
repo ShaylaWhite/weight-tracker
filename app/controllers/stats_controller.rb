fed6e2c261bd20e32a4940bfee7e4667502e085b
shayla_white <shayladwhite1@gmail.com>
2020-05-13 16:12:47 -0700

    fixed stats_controller and edit/delete routes

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,43 +1,49 @@
 class StatsController < ApplicationController 
+
+    get '/stats' do
+        @stats = Stats.all
+        erb :'stats/index'
+      end
+
+
   
     get '/stats/new' do
         if !Helpers.is_logged_in?(session)
           redirect '/'
         end
         erb :'stats/new'
-    end
-    post '/stats' do
-        stat = Stat.create(params)
+      end
+    
+      post '/stats' do
+        stats = Stat.create(params)
         user = Helpers.current_user(session)
         stat.user = user
         stat.save
         redirect to "/users/#{user.id}"
       end
-
+    
       get '/stats/:id' do
         if !Helpers.is_logged_in?(session)
           redirect '/'
         end
         @stat = Stat.find_by(id: params[:id])
-        if !@Stat
+        if !@stat
           redirect to '/'
         end
-        erb :'stats/show'
+        erb :'/stats/show'
       end
-
-
-
-
+    
+    
       get '/stats/:id/edit' do
-        @stat = Stat.find_by(id: params[:id])
-      if !Helpers.is_logged_in?(session) || !@stat || @stat.user != Helpers.current_user(session)
-        redirect '/'
+          @stat = Stat.find_by(id: params[:id])
+        if !Helpers.is_logged_in?(session) || !@stat || @stat.user != Helpers.current_user(session)
+          redirect '/'
+        end
+        erb :'/stats/edit'
       end
-      erb :'/stats/edit'
-    end
-
-    patch '/stats/:id' do
-        @stat = Stat.find_by(id: params[:id])
+    
+      patch '/stats/:id' do
+        stat = Stat.find_by(id: params[:id])
         if stat && stat.user == Helpers.current_user(session)
           stat.update(params[:stat])
           redirect to "/stats/#{stat.id}"
@@ -53,7 +59,4 @@ class StatsController < ApplicationController
         end
         redirect to '/stats'
       end
-    
-  
-
-end
+    end
